96f54dcbf68598555eef90f8c33e538471e1a374
zakirt <zakir-ny@hotmail.com>
2017-05-14 01:02:36 -0400

    Section 3: Added Boostrap library, created main compoents and layout.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'Just a test recipe', 'http://tinyurl.com/ka9wbev'),
+        new Recipe('Test Recipe', 'Just a test recipe', 'http://tinyurl.com/ka9wbev')
+    ];
+    
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
